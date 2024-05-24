3c7742179fe7687177070ae68981507233dbec37
sergeyxudyakow <81656454+sergeyxudyakow@users.noreply.github.com>
2024-05-24 19:57:21 +0300

    Add files via upload

--- script.js
+++ script.js
@@ -27,3 +27,14 @@ function toggleMenu() {
 }
 
 hamburger.addEventListener("click", toggleMenu)
+
+document.getElementById("cards").onmousemove = e => {
+    for(const card of document.getElementsByClassName("circle")) {
+      const rect = card.getBoundingClientRect(),
+            x = e.clientX - rect.left,
+            y = e.clientY - rect.top;
+  
+      card.style.setProperty("--mouse-x", `${x}px`);
+      card.style.setProperty("--mouse-y", `${y}px`);
+    };
+  }
